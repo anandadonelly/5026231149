c4f2c9a46ac9e90f60d5eca1b6aebd0e83802fab
Anandadonelly <anandadonelly@gmail.com>
2025-06-05 16:26:30 +0700

    Pertemuan 14

--- resources/views/Template.blade.php
+++ resources/views/Template.blade.php
@@ -1,63 +1,59 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>
+<html lang="en">
 
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<head>
+    <title>Ananda Donelly Reksana : 5026231149</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <style>
+        .jumbotron {
+            background-color: #9de2e2;
+            height: 100px;
+        }
+    </style>
+</head>
 
-        <!-- jQuery library -->
-        <script
-            src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+<body>
+    <div class="jumbotron text-center mx-auto">
+        <h1>5026231149 : Ananda Donelly Reksana</h1>
+    </div>
 
-        <!-- Popper JS -->
-        <script
-            src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <nav class="navbar navbar-expand-sm bg-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">All Front End</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/pegawai">Pegawai</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tugas CRUD</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">EAS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Latihan 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Latihan 2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Latihan 3</a>
+            </li>
+        </ul>
+    </nav>
+    <br>
+    <div class="container">
+        @yield('content')
+    </div>
 
-        <!-- Latest compiled JavaScript -->
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="jumbotron">
-                        <h1 class="display-3">Selamat Datang</h1>
-                        <p class="lead">Jumbo helper text</p>
-                        <hr class="my-2">
-                        <p>More info</p>
-                        <p class="lead">
-                            <a class="btn btn-primary btn-lg"
-                                href="Jumbo action link" role="button">Web ITS</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-2 bg-primary text-white">
-                        Kolom 1
-                    </div>
-                    <div class="col-4 bg-warning text-white">
-                        Kolom 2
-                        <div class="row">
-                            <div class="col-6 bg-dark text-white">
-                                Kolom 2.1
-                            </div>
-                            <div class="col bg-info text-white">
-                                Kolom 2.2
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 bg-success text-white">
-                        Kolom 3
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>
+</body>
+
 </html>
